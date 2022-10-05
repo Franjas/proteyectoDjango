318b1242df59ea27207b3d65ceb23d74fb4c8d70
franjas <franjas3@gmail.com>
2022-10-05 19:01:04 -0400

    commit

---
+++ todo/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Todo
+
+def index(request):
+    todos = Todo.objects.filter(title__contains = request.GET.get('search',''))
+    context = {
+        'todos': todos
+        }
+    return render(request, 'todo/index.html', context)
+
+def view(request, id):
+    return render(request, 'todo/index.html', {})
+
+def edit(request, id):
+    return render(request, 'todo/index.html', {})
+
+def create(request):
+    return render(request, 'todo/index.html', {})
+
+def delete(request, id):
+    return render(request, 'todo/index.html', {})
